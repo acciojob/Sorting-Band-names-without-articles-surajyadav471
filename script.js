4f58398e69440954ecdf03c3ce8a2a044eb6b04c
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-05-23 18:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+// Remove articles from band names
+const articles = ['a', 'an', 'the'];
+bandNames = bandNames.map(name => name.split(' ').filter(word => !articles.includes(word.toLowerCase())).join(' '));
+
+// Sort band names in lexicographic order
+bandNames.sort();
+let html = '<ul id="band">';
+bandNames.forEach(name => {
+  html += `<li>${name}</li>`;
+});
+html += '</ul>';
+
+console.log(html);
